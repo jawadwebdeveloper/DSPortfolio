8898fbf3266d5d0fc6b117ed5e5d7fcb28c246fc
jawadwebdeveloper <mrjd927@gmail.com>
2023-08-06 00:35:15 +0500

    first commit in DS Portfolio

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import Preloader from './components/Preloader/Preloader';
+
+import Routeing from './components/Routers/Routeing';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Projects from './pages/Projects/Projects';
+import Skills from './pages/Skills/Skills';
+
+function App() {
+  
+
+  let docTitle = document.title;
+  window.addEventListener("blur",()=>{
+    document.title="Plz Come Back🙄";
+  });
+  window.addEventListener("focus",()=>{
+    document.title = docTitle;
+  });
+
+
+
+  return (
+    <>
+    {/* <p class="example">Resize the browser window to see how the background color of this paragraph changes on different screen sizes.</p> */}
+        
+      
+      {/* <Preloader/> */}
+      <Routeing/>
+      <About/>
+      <Projects/>
+      <Skills/>
+      <Contact/>
+     
+    </>
+  );
+}
+
+export default App;
